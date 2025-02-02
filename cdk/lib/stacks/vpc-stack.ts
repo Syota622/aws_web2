9be31810c0d5fa625c185fd3d80999cf9d08b8d4
Syota0622 <wvp360gqanukcqhg@gmail.com>
2025-02-02 23:00:33 +0900

    New Create VPC

---
+++ cdk/lib/stacks/vpc-stack.ts
@@ -0,0 +1,34 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+
+export class VpcStack extends cdk.Stack {
+  public readonly vpc: ec2.Vpc;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // VPCの作成
+    this.vpc = new ec2.Vpc(this, 'MainVpc', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      maxAzs: 2,
+      
+      // サブネットの設定
+      subnetConfiguration: [
+        {
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+      ],
+      
+      // // NAT Gatewayの設定
+      // natGateways: 1,
+    });
+  }
+}
