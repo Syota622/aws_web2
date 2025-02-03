65bd380501d5f518a464a77566e40ee74e5f6ac5
Syota0622 <wvp360gqanukcqhg@gmail.com>
2025-02-03 22:46:49 +0900

    Change Name VPC

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
+import * as dotenv from 'dotenv';
 import * as cdk from 'aws-cdk-lib';
 import { VpcStack } from '../lib/stacks/vpc-stack';
 
+// .envファイルの読み込み
+dotenv.config();
+
 const app = new cdk.App();
-new VpcStack(app, 'VpcStack');
+
+const projectName = process.env.PROJECT_NAME || 'learn';
+const envName = process.env.ENV_NAME || 'prod';
+
+new VpcStack(app, `VpcStack-${projectName}-${envName}`, {
+  projectName,
+  envName,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
--- cdk/lib/stacks/vpc-stack.ts
+++ cdk/lib/stacks/vpc-stack.ts
@@ -2,35 +2,42 @@ import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
+interface VpcStackProps extends cdk.StackProps {
+  projectName: string;
+  envName: string;
+}
+
 export class VpcStack extends cdk.Stack {
   public readonly vpc: ec2.Vpc;
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: VpcStackProps) {
     super(scope, id, props);
 
-    // VPCの作成
-    this.vpc = new ec2.Vpc(this, 'MainVpc', {
+    const resourceName = `vpc-${props.projectName}-${props.envName}`;
+
+    this.vpc = new ec2.Vpc(this, resourceName, {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
       maxAzs: 2,
       
-      // サブネットの設定
       subnetConfiguration: [
         {
-          name: 'Public',
+          // サブネット名にも環境変数を反映
+          name: `Public`,
           subnetType: ec2.SubnetType.PUBLIC,
           cidrMask: 24,
         },
         {
-          name: 'Private',
+          name: `Private`,
           subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
           cidrMask: 24,
         },
       ],
-      
+
       // EIPとNAT Gatewayを無効化
       natGateways: 0,
+
       // デフォルトセキュリティグループの制限を無効化(インバウンド、アウトバウンドの削除)
-      restrictDefaultSecurityGroup: true
+      restrictDefaultSecurityGroup: false,
     });
   }
 }
