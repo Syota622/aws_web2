df50ece1f12de7dfc1c283b9426ff7790c8a30d5
Syota0622 <wvp360gqanukcqhg@gmail.com>
2025-03-12 22:03:32 +0900

    コマンド追加

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,9 +1,22 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import { Vpc } from "./src/network/vpc";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+// 設定を取得
+const config = new pulumi.Config();
+const projectName = config.get("projectName") || "pulumi-aws-infra";
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// 環境変数を取得（または設定）
+const env = config.get("environment") || "dev";
+
+// VPCの作成
+const mainVpc = new Vpc(`${projectName}-vpc-${env}`, {
+    cidrBlock: "10.0.0.0/16",
+    tags: {
+        Project: projectName,
+        Environment: env,
+    },
+});
+
+// 出力
+export const vpcId = mainVpc.vpc.id;
+export const vpcCidrBlock = mainVpc.vpc.cidrBlock;
