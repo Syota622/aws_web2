3e0cf3f4516fd1688e3a241288c262d3d0501aca
Syota0622 <wvp360gqanukcqhg@gmail.com>
2025-03-18 22:49:38 +0900

    git clone https://github.com/sst/monorepo-template.git

--- sst/sst.config.ts
+++ sst/sst.config.ts
@@ -3,11 +3,18 @@
 export default $config({
   app(input) {
     return {
-      name: "sst",
+      name: "monorepo-template",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
-  async run() {},
+  async run() {
+    const storage = await import("./infra/storage");
+    await import("./infra/api");
+
+    return {
+      MyBucket: storage.bucket.name,
+    };
+  },
 });
