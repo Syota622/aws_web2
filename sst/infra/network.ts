4a3b436c659840b7076ae1867197b58353f89bfe
Syota0622 <wvp360gqanukcqhg@gmail.com>
2025-03-23 23:37:37 +0900

    VPC情報をエクスポート

--- sst/infra/network.ts
+++ sst/infra/network.ts
@@ -2,3 +2,10 @@
 export const vpc = new sst.aws.Vpc("MyVpc", {
   az: 3
 });
+
+// VPC情報をエクスポート
+export const vpcInfo = {
+  id: vpc.id,
+  publicSubnets: vpc.publicSubnets,
+  privateSubnets: vpc.privateSubnets
+};
