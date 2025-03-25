d1eaf327e18db93b270353de1183cc5a8f6d04b0
Syota0622 <wvp360gqanukcqhg@gmail.com>
2025-03-25 23:24:22 +0900

    New Create SecurityGroup

--- sst/infra/network.ts
+++ sst/infra/network.ts
@@ -3,6 +3,32 @@ export const vpc = new sst.aws.Vpc("MyVpc", {
   az: 3
 });
 
+// カスタムのセキュリティグループを作成
+export const serviceSecurityGroup = new aws.ec2.SecurityGroup("ServiceSecurityGroup", {
+  vpcId: vpc.id,
+  description: "Allow HTTP traffic",
+  ingress: [
+    {
+      description: "HTTP from anywhere",
+      fromPort: 80,
+      toPort: 80,
+      protocol: "tcp",
+      cidrBlocks: ["0.0.0.0/0"]
+    }
+  ],
+  egress: [
+    {
+      fromPort: 0,
+      toPort: 0,
+      protocol: "-1",
+      cidrBlocks: ["0.0.0.0/0"]
+    }
+  ],
+  tags: {
+    Name: "ServiceSecurityGroup"
+  }
+});
+
 // VPC情報をエクスポート
 export const vpcInfo = {
   id: vpc.id,
