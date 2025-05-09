cc7c98e276a6611aba586792f83e0faf6377ac8f
Syota0622 <wvp360gqanukcqhg@gmail.com>
2025-05-09 13:56:55 +0900

    aurora

--- sst/infra/aurora.ts
+++ sst/infra/aurora.ts
@@ -1,12 +1,18 @@
 import { vpc } from "./vpc-network";
 
-// const postgres = new sst.aws.Aurora("MyDatabase", {
-//   engine: "postgres",
-//   scaling: {
-//     min: "1 ACU",
-//     max: "2 ACU"
-//   },
-//   vpc,
-//   // username: "postgres", // ユーザー名（直接指定する場合）
-//   // password: "mypassword", // パスワード（直接指定する場合）
-// });
+const postgres = new sst.aws.Aurora("MyDatabase", {
+  engine: "postgres",
+  scaling: {
+    min: "1 ACU",
+    max: "2 ACU"
+  },
+  vpc,
+  // username: "postgres", // ユーザー名（直接指定する場合）
+  // password: "mypassword", // パスワード（直接指定する場合）
+});
+
+// const aurora = new sst.aws.Postgres.v1("MyCluster",
+//   vpc: {
+//     privateSubnets
+//   }
+// )
