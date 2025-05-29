9aa39b8c955e3a4f8ef1430b3e3602d35ef490fb
Syota0622 <wvp360gqanukcqhg@gmail.com>
2025-05-29 12:46:01 +0900

    CodePiplineの構築に成功

--- sst/infra/pipline/expo_code_pipline.ts
+++ sst/infra/pipline/expo_code_pipline.ts
@@ -164,87 +164,87 @@ const expoCodeDeploy = new aws.codebuild.Project(
   }
 );
 
-// const expoCodePipeline = new aws.codepipeline.Pipeline(
-//   `${idPrefix}-${process.env.SST_STAGE || "dev"}`,
-//   {
-//     name: `${idPrefix}-${process.env.SST_STAGE || "dev"}`,
-//     piplineType: "V2",
-//     roleArn: codePipelineExpoRole.arn,
-//     artifactStores: [
-//       {
-//         type: "S3",
-//         location: artifactBucket.name,
-//         // encryptionKey: {
-//         //   id: artifactBucket.encryptionKey.id,
-//         //   type: "KMS"
-//         // }
-//       }
-//     ],
-//     triggers: [
-//       {
-//         gitConfiguration: {
-//           sourceActionName: "Source",
-//           pushes: [
-//             {
-//               branches: {
-//                 includes: ["main"]
-//               },
-//               filePaths: {
-//                 includes: [
-//                   "sst/cicd/**",
-//                   "sst/infra/**"
-//                 ],
-//               },
-//             },
-//           ],
-//         },
-//         providerType: "CodeStarSourceConnection"
-//       },
-//     ],
-//     stages: [
-//       {
-//         name: "Source",
-//         actions: [
-//           {
-//             name: "Source",
-//             category: "Source",
-//             owner: "AWS",
-//             provider: "CodeStarSourceConnection",
-//             version: "1",
-//             configuration: {
-//               ConnectionArn: "arn:aws:codeconnections:ap-northeast-1:235484765172:connection/98eaf6f6-2b18-4045-86a9-dfae7734cb7d",
-//               FullRepositoryId: "Syota0622/aws_web2",
-//               BranchName: "main",
-//             },
-//             outputArtifacts: ["SourceArtifact"],
-//           },
-//         ],
-//       },
-//       {
-//         name: "Deploy",
-//         actions: [
-//           {
-//             name: "Deploy",
-//             category: "Build",
-//             owner: "AWS",
-//             provider: "CodeBuild",
-//             version: "1",
-//             inputArtifacts: ["SourceArtifact"],
-//             configuration: {
-//               ProjectName: "expo-code-deploy"
-//             },
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// );
+const expoCodePipeline = new aws.codepipeline.Pipeline(
+  `${idPrefix}-${process.env.SST_STAGE || "dev"}`,
+  {
+    name: `${idPrefix}-${process.env.SST_STAGE || "dev"}`,
+    pipelineType: "V2",
+    roleArn: codePipelineExpoRole.arn,
+    artifactStores: [
+      {
+        type: "S3",
+        location: artifactBucket.name,
+        // encryptionKey: {
+        //   id: artifactBucket.encryptionKey.id,
+        //   type: "KMS"
+        // }
+      }
+    ],
+    triggers: [
+      {
+        gitConfiguration: {
+          sourceActionName: "Source",
+          pushes: [
+            {
+              branches: {
+                includes: ["main"]
+              },
+              filePaths: {
+                includes: [
+                  "sst/cicd/**",
+                  "sst/infra/**"
+                ],
+              },
+            },
+          ],
+        },
+        providerType: "CodeStarSourceConnection"
+      },
+    ],
+    stages: [
+      {
+        name: "Source",
+        actions: [
+          {
+            name: "Source",
+            category: "Source",
+            owner: "AWS",
+            provider: "CodeStarSourceConnection",
+            version: "1",
+            configuration: {
+              ConnectionArn: "arn:aws:codeconnections:ap-northeast-1:235484765172:connection/98eaf6f6-2b18-4045-86a9-dfae7734cb7d",
+              FullRepositoryId: "Syota0622/aws_web2",
+              BranchName: "main",
+            },
+            outputArtifacts: ["SourceArtifact"],
+          },
+        ],
+      },
+      {
+        name: "Deploy",
+        actions: [
+          {
+            name: "Deploy",
+            category: "Build",
+            owner: "AWS",
+            provider: "CodeBuild",
+            version: "1",
+            inputArtifacts: ["SourceArtifact"],
+            configuration: {
+              ProjectName: "expo-code-deploy"
+            },
+          },
+        ],
+      },
+    ],
+  },
+);
 
-// // エクスポート用のオブジェクト
-// export const expoCodePipelineExports = {
-//   codePipelineExpoRole,
-//   codeBuildExpoRole,
-//   artifactBucket,
-//   expoCodeDeploy,
-//   expoCodePipeline,
-// };
+// エクスポート用のオブジェクト
+export const expoCodePipelineExports = {
+  codePipelineExpoRole,
+  codeBuildExpoRole,
+  artifactBucket,
+  expoCodeDeploy,
+  expoCodePipeline,
+};
